357753edfcbf951703cb109046042908556b92ad
azka <azka24chaniago@gmail.com>
2020-10-20 21:08:30 +0700

    create base models, that necessary for the property system

--- property/models.py
+++ property/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class Customer(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, verbose_name="Akun"
+    )
+    address = models.TextField(verbose_name="Alamat")
+    email = models.EmailField(verbose_name="Email")
+    phone = models.CharField(
+        max_length=20, verbose_name="No Telepon / HP"
+    )
+    fullname = models.CharField(
+        max_length=100, verbose_name="Nama"
+    )
+
+    def __str__(self):
+        return self.fullname
+
+
+@receiver(post_save, sender=User)
+def create_user_customer(sender, instance, created, ** kwargs):
+    if created:
+        Customer.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_customer(sender, instance, **kwargs):
+    instance.customer.save()
+
+
+class Estate(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Nama")
+    lot_type = models.CharField(
+        max_length=50, verbose_name="Tipe Rumah"
+    )
+    lot_length = models.FloatField(verbose_name="Panjang")
+    lot_width = models.FloatField(verbose_name="Lebar")
+    price = models.FloatField(verbose_name="Harga")
+
+    def __str__(self):
+        return f"[{self.lot_type}] {self.name}"
+
+
+class EstateGallery(models.Model):
+    estate = models.ForeignKey(Estate, on_delete=models.CASCADE)
+    image = models.ImageField(verbose_name="Gambar")
+    description = models.CharField(
+        max_length=100, verbose_name="Deskripsi"
+    )
+
+    def __str__(self):
+        return f"{self.description} ({self.estate})"
+        
