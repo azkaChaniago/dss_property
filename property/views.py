55d9a25faf844a8920133b2f637955cf40433a51
azka <azka24chaniago@gmail.com>
2020-10-25 09:40:37 +0700

    copy static template files into static app folder and register the templates page with it

--- property/views.py
+++ property/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    """
+    docstring
+    """
+    templates = "home.html"
+    context = {
+        "title": "Welcome",
+        "menu": "home_menu"
+    }
+    return render(request, templates, context)
