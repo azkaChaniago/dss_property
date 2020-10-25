6377e46f9f0b633640d8676c86a1e2df36f6ee5e
azka <azka24chaniago@gmail.com>
2020-10-25 09:49:28 +0700

    create simple auth flow for accessing home page

--- property/urls.py
+++ property/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import home
+from .views import *
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+    path('login/', login_client, name="login_client"),
+    path('logout/', logout_client, name="logout_client"),
     path('home/', home, name="home"),
 ]
--- property/views.py
+++ property/views.py
@@ -1,12 +1,33 @@
-from django.shortcuts import render
+from django.contrib.auth import logout
+from django.shortcuts import render, redirect
+
+def login_client(request):
+    """
+    docstring
+    """
+    templates = "login.html"
+    context = {
+        "title": "Login",
+        "menu": "login_menu"
+    }
+    return render(request, templates, context)
+
 
 def home(request):
     """
     docstring
     """
+    if not request.user.is_authenticated:
+        return redirect('logout_client')
+
     templates = "home.html"
     context = {
         "title": "Welcome",
         "menu": "home_menu"
     }
     return render(request, templates, context)
+
+
+def logout_client(request):
+    logout(request)
+    return redirect("login_client")
