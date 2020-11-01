b283e8e01cb44ecb391a75619a5252859c197371
azka <azka24chaniago@gmail.com>
2020-11-01 08:18:29 +0700

    redefine and update forms

--- property/forms.py
+++ property/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from .models import Customer
+from .models import Customer, Estate, Profession
 
 class LoginForm(forms.Form):
     username = forms.CharField(
@@ -18,16 +18,13 @@ class LoginForm(forms.Form):
 
 
 class CustomerForm(forms.Form):
-    address = forms.CharField(
-        label='Alamat',
-        max_length=100,
-        widget=forms.TextInput()
+    LOAN_STATE = (
+        ("1", "Approval"),
+        ("2", "Penalty"),
+        ("3", "In Arrears"),
+        ("4", "Paid Off"),
     )
-    phone = forms.CharField(
-        label='No Telepon / HP',
-        max_length=100,
-        widget=forms.PasswordInput()
-    )
+    
     email = forms.CharField(
         label='Alamat email',
         max_length=100
@@ -36,6 +33,42 @@ class CustomerForm(forms.Form):
         label='Nama Lengkap',
         max_length=100
     )
+    password = forms.CharField(
+        label='Password',
+        max_length=255,
+        widget=forms.PasswordInput
+    )
+    address = forms.CharField(
+        label='Alamat',
+        max_length=100,
+        widget=forms.TextInput()
+    )
+    phone = forms.CharField(
+        label='No Telepon / HP',
+        max_length=100,
+    )
+    job_ktp = forms.ModelMultipleChoiceField(
+        queryset=Profession.objects.all()
+    )
+    job = forms.CharField(
+        label="Pekerjaan",
+        max_length=100
+    )
+    salary = forms.IntegerField(
+        label="Gaji / Upah",
+    )
+    on_loan = forms.BooleanField()
+    loan_state = forms.ChoiceField(
+        label="Status Angsuran",
+        choices=LOAN_STATE
+    )
 
+
+class EstateForm(forms.ModelForm):
     class Meta:
-        model = Customer
+        model = Estate
+        fields = [
+            "name", "lot_type", "lot_length", "lot_width",
+            "price", "description", "locations", "picture",
+            "bedroom", "bathroom"
+        ]
