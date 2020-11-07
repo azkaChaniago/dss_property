bb16661fd232a1c029cfa8840231f304f625aa2c
azka <azka24chaniago@gmail.com>
2020-11-07 10:53:23 +0700

    calculate price and area in home screen

--- property/templatetags/property_extras.py
+++ property/templatetags/property_extras.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.simple_tag()
 def calculate_area(length, width, *args, **kwargs):
     return f"{length * width}m\u00b2"
+
+@register.simple_tag()
+def calculate_price(price, *args, **kwargs):
+    return f"{round(price / 1000000, 2)}Juta"
