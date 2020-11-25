48ab2024cb30296ed5b7526a173803395101d667
azka <azka24chaniago@gmail.com>
2020-11-25 13:47:07 +0700

    remove django_heroku into bottom

--- dss_property/settings.py
+++ dss_property/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 import django_heroku
 
-django_heroku.settings(locals())
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -132,3 +130,5 @@ STATICFILES_DIRS = (
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = ''
+
+django_heroku.settings(locals())
