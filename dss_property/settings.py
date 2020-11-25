593dd30a1b0b5086e8e40538c4245032274e204a
Muhammad Fadhil Azka <azka24chaniago@gmail.com>
2020-11-25 11:20:15 +0700

    update staticfiles_dir

--- dss_property/settings.py
+++ dss_property/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'media/static'),
+    os.path.join(BASE_DIR, 'property/static'),
 )
 
 MEDIA_URL = '/media/'
